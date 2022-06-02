06e95e3e419f0ba21ec89032aa046758ec480109
ramanan1998 <ramananr2698095@gmail.com>
2022-06-02 23:33:43 +0530

    fixed the side bar navigation issue, and made smooth scrolling effect

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+
 const hamBtn = document.querySelector("#open");
 const crossBtn = document.querySelector("#close");
 const hamSlide = document.querySelector(".ham-slide");
+const navBtns = document.querySelectorAll(".nav-button a");
 
-const inputName = document.getElementById("name");
-const inputEmail = document.getElementById("email");
-const inputMessage = document.getElementById("message");
-const submitButton = document.getElementById("submit-button");
+// const inputName = document.getElementById("name");
+// const inputEmail = document.getElementById("email");
+// const inputMessage = document.getElementById("message");
+// const submitButton = document.getElementById("submit-button");
+
 
 function openbar(){
-    
     hamSlide.style.visibility = "visible";
-    hamSlide.style.width = "200px"; 
+    hamSlide.style.width = "300px"; 
     crossBtn.style.display = "block"; 
     hamBtn.style.display = "none";
 }
+
 
 function closebar(){
     hamSlide.style.width = "0"; 
@@ -21,3 +24,8 @@ function closebar(){
     hamBtn.style.display = "block";
     crossBtn.style.display = "none"; 
 }
+
+hamSlide.addEventListener("click", () => {
+    closebar();
+});
+
